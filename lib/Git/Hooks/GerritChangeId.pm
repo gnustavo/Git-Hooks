e8c31732fc964863c0f251dedb559d0973aa3e55
Gustavo L. de M. Chaves <gustavo@cpqd.com.br>
2012-12-16 08:56:26 -0200

    New plugin Git::Hooks::GerritChangeId.

---
+++ lib/Git/Hooks/GerritChangeId.pm
@@ -0,0 +1,253 @@
+#!/usr/bin/env perl
+
+# Copyright (C) 2012 by CPqD
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use 5.010;
+use utf8;
+use strict;
+use warnings;
+
+package Git::Hooks::GerritChangeId;
+# ABSTRACT: Git::Hooks plugin to insert a Change-Id in a commit message.
+
+use Git::Hooks qw/:DEFAULT :utils/;
+use File::Slurp;
+use File::Temp qw/tempfile/;
+use Error qw(:try);
+
+(my $HOOK = __PACKAGE__) =~ s/.*:://;
+
+#############
+# Grok hook configuration, check it and set defaults.
+
+my $Config = hook_config($HOOK);
+
+##########
+
+sub clean_message {
+    my ($msg) = @_;
+
+    # strip comment lines
+    $msg =~ s/^#.*\n?//mg;
+
+    # strip Signed-of-by lines
+    $msg =~ s/^Signed-off-by:.*\n?//img;
+
+    # strip trailing whitespace from all lines
+    $msg =~ s/\s+$//mg;
+
+    # collapse multiple consecutive empty lines
+    $msg =~ s/\n{3,}/\n\n/sg;
+
+    # remove empty lines from the begining
+    $msg =~ s/^\n+//;
+
+    return '' unless length $msg;
+
+    # remove empty lines from the end
+    $msg =~ s/\n{2,}$/\n/;
+
+    return $msg;
+}
+
+sub gen_change_id {
+    my ($git, $msg) = @_;
+
+    my ($fh, $filename) = tempfile(undef, UNLINK => 1);
+
+    foreach my $info (
+        [ tree      => [qw/write-tree/] ],
+        [ parent    => [qw/rev-parse HEAD^0/] ],
+        [ author    => [qw/var GIT_AUTHOR_IDENT/] ],
+        [ committer => [qw/var GIT_COMMITTER_IDENT/] ],
+    ) {
+        try {
+            $fh->print($info->[0], ' ', $git->command($info->[1], {STDERR => 0}), "\n");
+        } otherwise {
+            # Can't find info. That's ok.
+        };
+    }
+
+    $fh->print("\n", $msg);
+    $fh->close();
+
+    return $git->hash_object(commit => $filename);
+}
+
+sub insert_change_id {
+    my ($git, $msg) = @_;
+
+    # Strip the patch data from the message.
+    $msg =~ s:^diff --git a/.*::ms;
+
+    # Does Change-Id: already exist? if so, exit (no change).
+    return if $msg =~ /^Change-id:/im;
+
+    # If the message is just blank space, exit.
+    my $clean_msg = clean_message($msg);
+    return unless length $clean_msg;
+
+    # strip comment lines
+    $msg =~ s/^#.*\n?//mg;
+
+    # Split $msg in interleaved blocks of text and empty-lines
+    my @blocks = split /(?<=\n)(\n+)/s, $msg;
+
+    # strip a possible trailing empty line
+    pop @blocks if $blocks[-1] =~ /^\n+$/;
+
+    # Check if the last block is a footer
+    my $has_footer;
+    if (@blocks < 2) {
+        $has_footer = 0;
+    } else {
+        $has_footer = 1;
+        my $in_footer_comment = 0;
+        foreach (split /^/m, $blocks[-1]) {
+            if ($in_footer_comment) {
+                $in_footer_comment = 0 if /\]$/;
+            } elsif (/^\[[\w-]+:/i) {
+                $in_footer_comment = 1;
+            } elsif (! /^[\w-]+:/i) {
+                $has_footer = 0;
+                last;
+            }
+        }
+    }
+
+    # Build the Change-Id line.
+    my $change_id = 'Change-Id: I' . gen_change_id($git, $clean_msg) . "\n";
+
+    if ($has_footer) {
+	# Try to insert the change-id line after leading Bug|Issue
+	# lines in the footer.
+	my $inserted = 0;
+        my $where = 0;
+	while ($blocks[-1] =~ /^([\w-]+?):.*/gim) {
+            if ($1 =~ /^Bug|Issue$/i) {
+                $where = pos($blocks[-1]);
+            } else {
+                substr $blocks[-1], $where, 0, $change_id;
+                $inserted = 1;
+                last;
+            }
+	}
+	$blocks[-1] .= $change_id unless $inserted;
+    } else {
+	# Write the change-id in a new footer
+	push @blocks, "\n$change_id";
+    }
+
+    return join('', @blocks);
+};
+
+sub rewrite_message {
+    my ($git, $commit_msg_file) = @_;
+
+    my $msg = read_file($commit_msg_file);
+    defined $msg or die "$HOOK: Can't open file '$commit_msg_file' for reading: $!\n";
+
+    my $new_msg = insert_change_id($git, $msg);
+
+    # Rewrite the message file
+    write_file($commit_msg_file, $new_msg)
+	if defined $new_msg && $new_msg ne $msg;
+
+    return;
+}
+
+# Install hooks
+COMMIT_MSG \&rewrite_message;
+
+1;
+
+
+__END__
+=for Pod::Coverage clean_message gen_change_id insert_change_id
+
+=head1 NAME
+
+Git::Hooks::GerritChangeId - Git::Hooks plugin to insert a Change-Id in a commit message.
+
+=head1 DESCRIPTION
+
+This Git::Hooks plugin is a reimplementation of Gerrit's official
+commit-msg hook for inserting change-ids in git commit messages. (What
+follows is a partial copy of that document's DESCRIPTION section.)
+
+This plugin automatically inserts a globally unique Change-Id tag in
+the footer of a commit message. When present, Gerrit uses this tag to
+track commits across cherry-picks and rebases.
+
+After the hook has been installed in the user's local Git repository
+for a project, the hook will modify a commit message such as:
+
+    Improve foo widget by attaching a bar.
+    
+    We want a bar, because it improves the foo by providing more
+    wizbangery to the dowhatimeanery.
+    
+    Signed-off-by: A. U. Thor <author@example.com>
+
+by inserting a new C<Change-Id: > line in the footer:
+
+    Improve foo widget by attaching a bar.
+    
+    We want a bar, because it improves the foo by providing more
+    wizbangery to the dowhatimeanery.
+    
+    Change-Id: Ic8aaa0728a43936cd4c6e1ed590e01ba8f0fbf5b
+    Signed-off-by: A. U. Thor <author@example.com>
+
+The hook implementation is reasonably intelligent at inserting the
+Change-Id line before any Signed-off-by or Acked-by lines placed at
+the end of the commit message by the author, but if no such lines are
+present then it will just insert a blank line, and add the Change-Id
+at the bottom of the message.
+
+If a Change-Id line is already present in the message footer, the
+script will do nothing, leaving the existing Change-Id
+unmodified. This permits amending an existing commit, or allows the
+user to insert the Change-Id manually after copying it from an
+existing change viewed on the web.
+
+To enable the plugin you should define the appropriate Git
+configuration option like this:
+
+    git config --add githooks.commit-msg  GerritChangeId
+
+=head1 CONFIGURATION
+
+There's no configuration needed or provided.
+
+=head1 EXPORTS
+
+This module exports one routine that can be used directly without
+using all of Git::Hooks infrastructure.
+
+=head2 rewrite_message GIT, MSGFILE
+
+This is the routine used to implement the C<commit-msg> hook. It needs
+a C<Git::More> object and the name of a file containing the commit
+message.
+
+=head1 REFERENCES
+
+Gerrit's Home Page: L<http://gerrit.googlecode.com/>
+
+Gerrit's official commit-msg hook: L<https://gerrit.googlesource.com/gerrit/+/master/gerrit-server/src/main/resources/com/google/gerrit/server/tools/root/hooks/commit-msg>
+
+Gerrit's official hook documentation: L<https://gerrit.googlesource.com/gerrit/+/master/Documentation/cmd-hook-commit-msg.txt>
---
+++ t/02-gerrit-change-id.t
@@ -0,0 +1,163 @@
+# -*- cperl -*-
+
+use 5.010;
+use strict;
+use warnings;
+use lib 't';
+use Test::More tests => 44;
+use Cwd;
+use File::Slurp;
+use File::Temp qw/tmpnam/;
+use Git::Hooks::GerritChangeId;
+
+require "test-functions.pl";
+
+my ($repo, $filename) = new_repos();
+
+install_hooks($repo);
+
+sub last_log {
+    return $repo->get_commit_msg('HEAD');
+}
+
+sub diag_last_log {
+    my $last_log = last_log();
+    diag(" LAST LOG[", length($last_log), "]<<<$last_log>>>\n");
+}
+
+my $msgfile = tmpnam();
+
+sub cannot_commit {
+    my ($testname, $regex, $msg) = @_;
+    append_file($filename, "new line\n");
+    $repo->command(add => $filename);
+    write_file($msgfile, $msg);
+    unless (test_nok_match($testname, $regex, $repo, 'commit', '-F', $msgfile)) {
+	diag_last_log();
+    }
+}
+
+sub can_commit {
+    my ($testname, $msg) = @_;
+    append_file($filename, "new line\n");
+    $repo->command(add => $filename);
+    write_file($msgfile, $msg);
+    return test_ok("$testname [commit]", $repo, 'commit', '-F', $msgfile);
+}
+
+
+$repo->command(config => "githooks.commit-msg", 'gerrit-change-id');
+
+# test EmptyMessages
+foreach my $test (
+    [ empty     => "" ],
+    [ space     => " " ],
+    [ newline   => "\n" ],
+    [ newlines  => "\n\n" ],
+    [ sp_nl_sp  => " \n " ],
+) {
+    cannot_commit("empty: $test->[0]", qr/Aborting commit due to empty commit message/, $test->[1]);
+}
+
+# test CommentOnlyMessages
+foreach my $test (
+    [ comment   => "#" ],
+    [ com_nl    => "#\n" ],
+    [ comments  => "# on branch master\n# Untracked files:\n" ],
+    [ nl_coms   => "\n# on branch master\n# Untracked files:\n" ],
+    [ nlnl_coms => "\n\n# on branch master\n# Untracked files:\n" ],
+    [ patch     => "\n# on branch master\ndiff --git a/src b/src\nnew file mode 100644\nindex 0000000..c78b7f0\n" ],
+) {
+    if (can_commit("comment: $test->[0]", $test->[1])) {
+	if (last_log() !~ /Change-Id:/i) {
+	    pass("comment: $test->[0] (msg ok)");
+	} else {
+	    fail("comment: $test->[0] (msg ok)");
+	    diag_last_log();
+	}
+    } else {
+	fail("comment: $test->[0] (msg fail)");
+    }
+}
+
+# test ChangeIdAlreadySet
+foreach my $test (
+    [ 'set a'             => "a\n\nChange-Id: Iaeac9b4149291060228ef0154db2985a31111335\n" ],
+    [ 'set fix'           => "fix: this thing\n\nChange-Id: I388bdaf52ed05b55e62a22d0a20d2c1ae0d33e7e\n" ],
+    [ 'set fix-a-widget:' => "fix-a-widget: this thing\n\nChange-Id: Id3bc5359d768a6400450283e12bdfb6cd135ea4b\n" ],
+    [ 'set FIX'           => "FIX: this thing\n\nChange-Id: I1b55098b5a2cce0b3f3da783dda50d5f79f873fa\n" ],
+    [ 'set Fix-A-Widget'  => "Fix-A-Widget: this thing\n\nChange-Id: I4f4e2e1e8568ddc1509baecb8c1270a1fb4b6da7\n" ],
+) {
+    if (can_commit("preset: $test->[0]", $test->[1])) {
+	if (last_log() eq $test->[1]) {
+	    pass("preset: $test->[0] (msg ok)");
+	} else {
+	    fail("preset: $test->[0] (msg ok)");
+	    diag_last_log();
+	}
+    } else {
+	fail("preset: $test->[0] (msg fail)");
+    }
+}
+
+
+# Check how the official hook change the message
+sub expected {
+    my ($msg) = @_;
+
+    write_file($msgfile, $msg);
+
+    my $dir = getcwd;
+    chdir $repo->repo_path();
+    system(catfile($dir, 't', 'gerrit-commit-msg.sh'), $msgfile);
+    chdir $dir;
+
+    return read_file($msgfile);
+}
+
+sub produced {
+    my ($msg) = @_;
+
+    # Check how our hook change the message
+    write_file($msgfile, $msg);
+    Git::Hooks::GerritChangeId::rewrite_message($repo, $msgfile);
+    read_file($msgfile);
+}
+
+sub compare {
+    my ($testname, $expected, $produced) = @_;
+    if ($produced eq $expected) {
+	pass($testname);
+    } else {
+	fail($testname);
+	diag("Expected=<<<$expected>>>\nProduced=<<<$produced>>>");
+    }
+}
+
+my $CID  = 'Change-Id: I7fc3876fee63c766a2063df97fbe04a2dddd8d7c';
+my $SOB1 = 'Signed-off-by: J Author <ja@example.com>';
+my $SOB2 = 'Signed-off-by: J Committer <jc@example.com>';
+
+foreach my $test (
+    [ 'no-CID',              "a\n" ],
+    [ 'single-line',         "a\n\n$CID\n" ],
+    [ 'multi-line',          "a\n\nb\n\nc\n\n$CID\n" ],
+    [ 'not-a-footer',        "a\n\nb: not a footer\nc\nd\ne\n\nf\ng\nh\n\n$CID\n" ],
+    [ 'single-line SOB',     "a\n\n$CID\n$SOB1\n" ],
+    [ 'multi-line SOB',      "a\n\nb\n\nc\n\n$CID\n$SOB1\n" ],
+    [ 'not-a-footer SOB',    "a\n\nb: not a footer\nc\nd\ne\n\nf\ng\nh\n\n$CID\n$SOB1\n" ],
+    [ 'note-in-middle',      "a\n\nNOTE: This\ndoes not fix it.\n\n$CID\n" ],
+    [ 'kernel-style-footer', "a\n\n$CID\n$SOB1\n[ja: Fixed\n     the indentation]\n$SOB2\n" ],
+    [ 'CID-after-Bug',       "a\n\nBug: 42\n$CID\n$SOB1\n" ],
+    [ 'CID-after-Issue',     "a\n\nIssue: 42\n$CID\n$SOB1\n" ],
+    [ 'commit-dashv',        "a\n\n$SOB1\n$SOB2\n\n# on branch master\ndiff --git a/src b/src\nnew file mode 100644\nindex 0000000..c78b7f0\n" ],
+    [ 'with-url http',       "a\n\nhttp://example.com/ fixes this\n\n$CID\n" ],
+    [ 'with-url https',      "a\n\nhttps://example.com/ fixes this\n\n$CID\n" ],
+    [ 'with-url ftp',        "a\n\nftp://example.com/ fixes this\n\n$CID\n" ],
+    [ 'with-url git',        "a\n\ngit://example.com/ fixes this\n\n$CID\n" ],
+    [ 'with-false-tags',     "foo\n\nFakeLine:\n  foo\n  bar\n\n$CID\nRealTag: abc\n" ],
+) {
+    my $expected = expected($test->[1]);
+    my $produced = produced($test->[1]);
+    compare("compare: $test->[0]", $expected, $produced);
+}
