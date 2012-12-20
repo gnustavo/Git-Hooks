9ed6c2d8ac5280aa02b740c2031de0b68fac8773
Gustavo L. de M. Chaves <gustavo@cpqd.com.br>
2012-12-20 19:51:24 -0200

    Move around use pragmas to placate Perl::Critic

--- lib/Git/Hooks/GerritChangeId.pm
+++ lib/Git/Hooks/GerritChangeId.pm
@@ -15,14 +15,13 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+package Git::Hooks::GerritChangeId;
+# ABSTRACT: Git::Hooks plugin to insert a Change-Id in a commit message.
+
 use 5.010;
 use utf8;
 use strict;
 use warnings;
-
-package Git::Hooks::GerritChangeId;
-# ABSTRACT: Git::Hooks plugin to insert a Change-Id in a commit message.
-
 use Git::Hooks qw/:DEFAULT :utils/;
 use File::Slurp;
 use File::Temp qw/tempfile/;
